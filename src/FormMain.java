5686668f62b54f853818c15c7764f5e199a906c3
BcemXaxa <artemderk@mail.ru>
2024-03-14 00:34:33 +0300

    Trying to implement interface

---
+++ src/FormMain.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+
+public class FormMain {
+	private JTabbedPane tabbedPane;
+	private JPanel panel1;
+	private JPanel panelMatrix;
+	private JPanel panelSoLE;
+	private JPanel panelMultiplication;
+	private JTable table1;
+	private JTable table2;
+	private JButton button1;
+	private JButton button2;
+	private JButton button3;
+	private JComboBox comboBox1;
+	private JButton button4;
+	private JButton button5;
+	private JTable table3;
+	private JTable table4;
+	private JTable table5;
+	private JTable table6;
+	private JButton button6;
+	private JButton button7;
+	private JButton button8;
+	private JButton button9;
+	private JTable table7;
+	private JButton button10;
+	private JComboBox comboBox2;
+	private JButton button11;
+	private JButton button12;
+	private JButton button13;
+	private JButton button14;
+	private JButton button15;
+	private JButton button16;
+	private JButton button17;
+}
